5d1faa9ba8fb0b316a7735ca2d337f4099d21448
88guri <quffl2002@naver.com>
2025-10-31 18:26:27 +0900

    [Refact] MemberController에 공통 예외 및 응답 처리 구조 적용

--- src/main/java/org/sopt/controller/MemberController.java
+++ src/main/java/org/sopt/controller/MemberController.java
@@ -3,13 +3,17 @@ package org.sopt.controller;
 import org.sopt.domain.Member;
 import org.sopt.dto.request.MemberCreateRequestDto;
 import org.sopt.dto.response.MemberInfoResponseDto;
+import org.sopt.global.constant.MemberErrorCode;
+import org.sopt.global.exception.MemberException;
+import org.sopt.global.response.GlobalBaseResponse;
 import org.sopt.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static org.sopt.global.constant.MemberSuccessCode.DELETED_MEMBER;
 
 @RestController
 public class MemberController {
@@ -21,7 +25,7 @@ public class MemberController {
     }
 
     @PostMapping("/users")
-    public MemberInfoResponseDto createMember(@RequestBody MemberCreateRequestDto requestDto) {
+    public GlobalBaseResponse<MemberInfoResponseDto> createMember(@RequestBody MemberCreateRequestDto requestDto) {
         Long memberId = memberService.join(
                 requestDto.name(),
                 requestDto.birth(),
@@ -29,28 +33,33 @@ public class MemberController {
                 requestDto.gender()
         );
 
-        Optional<Member> created = memberService.findOne(memberId);
-        return created.map(MemberInfoResponseDto::from).orElse(null);
+        MemberInfoResponseDto response = memberService.findOne(memberId)
+                .map(MemberInfoResponseDto::from)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.INVALID_NAME));
+
+        return GlobalBaseResponse.ok(response);
     }
 
     @GetMapping("/users/{id}")
-    public MemberInfoResponseDto findMemberById(@PathVariable Long id) {
+    public GlobalBaseResponse<MemberInfoResponseDto> findMemberById(@PathVariable Long id) {
         Member member = memberService.findOne(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 회원이 존재하지 않습니다."));
-        return MemberInfoResponseDto.from(member);
+                .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+        return GlobalBaseResponse.ok(MemberInfoResponseDto.from(member));
     }
 
-    @GetMapping("/users/all")
-    public List<MemberInfoResponseDto> getAllMembers() {
-        return memberService.findAllMembers()
+    @GetMapping("/all")
+    public GlobalBaseResponse<List<MemberInfoResponseDto>> getAllMembers() {
+        List<MemberInfoResponseDto> members = memberService.findAllMembers()
                 .stream()
                 .map(MemberInfoResponseDto::from)
                 .collect(Collectors.toList());
+        return GlobalBaseResponse.ok(members);
     }
 
-    @DeleteMapping("/users/{id}")
-    public String deleteMember(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public GlobalBaseResponse<String> deleteMember(@PathVariable Long id) {
         memberService.deleteMember(id);
-        return "회원이 성공적으로 삭제되었습니다.";
+        return GlobalBaseResponse.ok(DELETED_MEMBER);
     }
 }
